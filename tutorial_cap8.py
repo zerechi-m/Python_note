fa3c5d2a607fd9016fc8dd71460f7b6fce2a1ee8
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-04-12 16:36:37 +0900

    8.3 例外の処理
    - try節とexcept節
    - except節にエラーの種類を記載することで任意の処理ができる
    - except節にelseを使用することで
     例外に該当しない場合の処理を記述

--- tutorial_cap8.py
+++ tutorial_cap8.py
@@ -19,7 +19,7 @@
 
 while True:
     try:
-        x = int(input('数字を入力してください:'))
+        # x = int(input('数字を入力してください:'))
         break
     except ValueError:
         print('これは有効な数字ではありません')
@@ -31,3 +31,66 @@ while True:
     # 発生したexcept文の条件式と一致すればexcept節が実行されて、その後try文の処理へと移る
     # 例外の型がexcept節と一致しないと送り出された例外はさらに外側にあるtry文に渡される
     # ハンドラが見つからないと 未処理例外 となってエラーが発生する
+  
+  #except節には 複数のバンドラを設ける事ができる
+
+   # except (RuntimeError, TypeError, NameError) のように
+
+class B(Exception):
+    pass
+
+class C(B):
+    pass
+
+class D(C):
+    pass
+
+for cls in (B,C,D):
+    try:
+        raise cls()
+    except D:
+        print('D')
+    except C:
+        print('C')
+    except B:
+        print('B')
+
+ # except節に何も記載しないことで、ワイルドカード(全エラー)にすることができる
+import sys 
+
+try:
+    f = open('text.txt', 'r')
+    s = f.readline()
+    i = int(s.strip())
+except OSError as err:
+    print('OS error: {0}'.format(err))
+except ValueError:
+    print('データが整数に変換できません')
+except:
+    print('予期せぬエラー:', sys.exc_info()[0])   # <--- ただし、デバッグが難しくなるので使用に気をつける
+    raise
+
+ # except節にelse文の使用方法
+  # elseを用いることにより、例外が発生しない場合のみ、コードを記載できる
+
+for arg in sys.argv[1:]:
+    try:
+        f = open(arg, 'r')           #<-- try節にコードを追加するよりも else節に記載
+    except OSError:
+        print('cannot open', arg)
+    else:
+        print(arg, 'has', len(f.readlines()), 'Lines')  #<-- 例外処理が発生しない場合のみelseが読まれ処理が行われる
+        f.close()
+
+ # 発生した例外の引数の引き出しついて
+
+try:
+    raise Exception('spam', 'eggs')
+except Exception as inst:
+    print(type(inst))          #<-- 例外インスタンスの型
+    print(inst.args)           #<-- .argsに格納された引数
+
+    print(inst)                #<-- __str_により引数は直接表示可能であるが、これは例外のサブクラスでオーバーライドされ得る引数のアンパック
+    x, y = inst.args           #<-- inst.argsを展開
+    print('x = ', x)
+    print('y = ', y)
