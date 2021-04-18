8da3b1752493db7d2e150347838f811e56ddb86e
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-04-18 17:18:32 +0900

    10.2 ファイルのワイルドカード

--- tutorial_cap10.py
+++ tutorial_cap10.py
@@ -9,11 +9,19 @@ print(os.getcwd())  # カレントディレクトリを返す
 
 # os.system('mkdir today') # カレントディレクトリにシステム側のシェルでmkdirを実行＊フォルダ作成
 
-print(dir(os))  # import os のモジュールの関数が全て入ったリストを返す。
+# print(dir(os))  # import os のモジュールの関数が全て入ったリストを返す。
 
-print(help(os)) # モジュールのdocstringから生成された詳細なマニュアルを返す
+# print(help(os)) # モジュールのdocstringから生成された詳細なマニュアルを返す
 
  # 日々のファイルやディレクトリの管理には shutil モジュールによる使いやすい高水準のインタフェースが良い
 
 # import shutil
 # shutil.copyfile('data.db', 'archive.db')
+
+# 10.2 ) ファイルのワイルドカード
+
+ # globモジュールはディレクトリをワイルドカード検索してファイル名のリストを返す関数を提供する。
+
+import glob
+print(glob.glob('*.py'))  # .pyの拡張子ファイルを全て検索して返す。
+
