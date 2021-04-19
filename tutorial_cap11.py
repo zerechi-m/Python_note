fd229a65a99fe1b4a5dc03d1e2595435a26076bf
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-04-19 12:48:30 +0900

    11.2 テンプレート

--- tutorial_cap11.py
+++ tutorial_cap11.py
@@ -36,3 +36,44 @@ x = 1234567.8
 print(locale.format("%d", x, grouping=True))
 
 print(locale.format_string("%s%.*f", (conv['currency_symbol'], conv['frac_digits'], x), grouping=True))
+
+
+# 11.2 ) テンプレート
+
+ # stringモジュールにはエンドユーザーが編集するのに向いた構文 Templateクラスがあるこれを使用すればアプリを書き換えずにカスタマイズできる
+ # 整形ではプレースホルダーとして$の後ろにPythonで有効な識別子をつけた物を使用する
+
+from string import Template
+t = Template('${village}folk send $$10 to $cause.')     # ${} か $ の後ろに文字列をおく $$で$文字がエスケープされる
+print(t.substitute(village='Nottingham', cause='the ditch fund'))
+
+ # substituteメソッドはプレースホルダーをディクショナリかキーワード引数で渡さないとKeyErrorを送り出す。
+ # こうした置き換えスタイルのアプリケーションではユーザーからのデータは不完全かもしれないので、safe_substituteメソッドを使用した方がいいかもしれない
+
+
+n = Template('Return the $item to $owner')
+d = dict(item='unladen swallow')           # $owner のキーに該当する値を渡してないのでkeyエラーになる
+# print(n.substitute(d))
+
+print(n.safe_substitute(d))                # safe_substitute を使用すると $owner キーがないと $ownerのまま出力される
+
+ # Templateのサブクラスでは区切り文字デリミタを変えられる
+ # 例えば以下のようなフォトブラウザ向けバッチ処理リネームユーティリティでは現在の日付・画像番号・ファイル形式などのプレースホルダーに%を使用しい
+
+import time, os.path
+
+photofiles = ['img_1074.jpg', 'img_1076.jpg', 'img_1077.jpg']
+
+class BatchRename(Template):
+    delimiter = '%'
+
+fmt = input('どのようにリネームしますか (%d-日付 %n-番号 %f-形式):')  # 入力数値 yud_%n_%f
+
+t = BatchRename(fmt)
+date = time.strftime('%d%b%y')
+for i, filename in enumerate(photofiles):
+    base, ext = os.path.splitext(filename)
+    newname = t.substitute(d=date, n=i, f=ext)
+    print('{0} --> {1}'.format(filename, newname))
+
+# 11.3 ) バイナリデータレコードの処理
